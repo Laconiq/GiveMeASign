462d7fe488d0487f354d648a3ba177c0dd191827
Laconiq <laconiqofficial@gmail.com>
2024-01-24 13:09:49 +0100

    Create Trigger Zone event

---
+++ Assets/Scripts/Progressions/TriggerZoneEvent.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneEvent : MonoBehaviour
+{
+    [SerializeField] private List<ProgressionScriptableObject> progressionsToUnlock;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) 
+            return;
+        foreach (ProgressionScriptableObject progression in progressionsToUnlock)
+        {
+            progression.IsProgressionFinished = true;
+            Debug.Log("Progression " + progression.name + " unlocked");
+        }
+    }
+}
