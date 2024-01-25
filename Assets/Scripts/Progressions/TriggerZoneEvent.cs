c5f5f158f50668bd7dbfd7c626e2e6261699b6b6
Laconiq <laconiqofficial@gmail.com>
2024-01-25 15:26:14 +0100

    Add TP to trigger zone

--- Assets/Scripts/Progressions/TriggerZoneEvent.cs
+++ Assets/Scripts/Progressions/TriggerZoneEvent.cs
@@ -1,14 +1,23 @@
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class TriggerZoneEvent : MonoBehaviour
 {
-    [SerializeField] private List<ProgressionScriptableObject> progressionsToUnlock;
+    [SerializeField] private bool canUnlockProgression;
+    [SerializeField, ShowIf("canUnlockProgression")] private List<ProgressionScriptableObject> progressionsToUnlock;
+    [SerializeField] private bool canTeleportPlayer;
+    [SerializeField, ShowIf("canTeleportPlayer")] private Transform teleportPosition;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player")) 
-            return;
-        foreach (ProgressionScriptableObject progression in progressionsToUnlock)
-            progression.IsProgressionFinished = true;
+        //Progression
+        if (other.CompareTag("Player") && canUnlockProgression)
+            foreach (ProgressionScriptableObject progression in progressionsToUnlock)
+                progression.IsProgressionFinished = true;
+
+        //Teleport
+        if (other.CompareTag("Player") && canTeleportPlayer)
+            other.transform.position = teleportPosition.position;
     }
 }
