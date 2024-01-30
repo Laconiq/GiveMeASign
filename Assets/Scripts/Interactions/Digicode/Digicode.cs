c44946a19ce46d34b48918a831f6ac9713d8c152
Laconiq <laconiqofficial@gmail.com>
2024-01-30 21:35:07 +0100

    Work on digicode feedback

--- Assets/Scripts/Interactions/Digicode/Digicode.cs
+++ Assets/Scripts/Interactions/Digicode/Digicode.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -6,24 +7,40 @@ public class Digicode : MonoBehaviour
 {
     [FormerlySerializedAs("progressionToUnlock")] [SerializeField] private Event eventToUnlock;
     private readonly List<DigicodeButton> _buttons = new List<DigicodeButton>();
+    [SerializeField] private TMP_Text passwordText;
+    [SerializeField] private AK.Wwise.Event wrongPasswordSound;
+    [SerializeField] private AK.Wwise.Event successSound;
+    
     public void PressButton(DigicodeButton button)
     {
+        if (_buttons.Count == 6)
+            return;
         _buttons.Add(button);
+        passwordText.text += "*";
     }
     
     public void CheckPassword()
     {
         if (_buttons.Count != 4)
-        {
-            Debug.Log("Wrong password");
-            _buttons.Clear();
-        }
+            OnFail();
         else
-            UnlockDoor();
+            OnSuccess();
     }
     
-    private void UnlockDoor()
+    private void OnSuccess()
     {
+        Debug.Log("Success");
         eventToUnlock.SetProgressionStatus(true);
+        passwordText.text = "";
+        _buttons.Clear();
+        successSound.Post(gameObject);
+    }
+    
+    private void OnFail()
+    {
+        Debug.Log("Wrong password");
+        passwordText.text = "";
+        _buttons.Clear();
+        wrongPasswordSound.Post(gameObject);
     }
 }
